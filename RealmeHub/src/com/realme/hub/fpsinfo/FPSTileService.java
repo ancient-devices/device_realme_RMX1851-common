goshawk22/android_device_realme_sdm710-common

sdm710-common: KharaMeParts: introduce FPS counter QS Tile
Squash of the following:

introduce FPS overlay

adapt FPS Info based on exposed data from kernel

* Current kernel emits measured_fps inside /sys/class/drm/card0/sde-crtc-0.
  It also has a different output format than the original patches.
  So adapt it so measured fps can be displayed properly.

* Shift position by 50px more so the fps counter is 100%
  visible without getting cutted by the hardware corner.

* Also, cleanup some unused imports and redundant file reader since
  we're already has that inside utils/.

Add FPS overlay QS tile

Digital-style icon for FPSInfo QS Tile

FPSInfoService: Switch to isDreaming check

Co-authored-by: Marko Man <darkobas@gmail.com>
Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
Signed-off-by: raghavt20 <raghavtandon2000@gmail.com>
Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

KharaMeParts/src/com/kharame/kharameparts/fpsinfo/FPSTileService.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (C) 2020 DerpFest ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kharame.kharameparts.fpsinfo;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+// TODO: Add FPS drawables
+public class FPSTileService extends TileService {
+
+  private final String KEY_FPS_INFO = "fps_info";
+
+  private boolean isShowing = false;
+
+  public FPSTileService() { }
+
+  @Override
+  public void onStartListening() {
+      super.onStartListening();
+      ActivityManager manager =
+              (ActivityManager) getSystemService(this.ACTIVITY_SERVICE);
+      for (ActivityManager.RunningServiceInfo service :
+              manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (FPSInfoService.class.getName().equals(
+                  service.service.getClassName())) {
+              isShowing = true;
+          }
+      }
+      updateTile();
+  }
+
+  @Override
+  public void onClick() {
+      Intent fpsinfo = new Intent(this, FPSInfoService.class);
+      if (!isShowing)
+          this.startService(fpsinfo);
+      else
+          this.stopService(fpsinfo);
+      isShowing = !isShowing;
+      updateTile();
+  }
+
+  private void updateTile() {
+      final Tile tile = getQsTile();
+      tile.setState(isShowing ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+      tile.updateTile();
+  }
+
+}

KharaMeParts/src/com/kharame/kharameparts/util/FileUtils.java
@@ -1,0 +1,160 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kharame.kharameparts.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class FileUtils {
+    private static final String TAG = "FileUtils";
+
+    private FileUtils() {
+        // This class is not supposed to be instantiated
+    }
+
+    /**
+     * Reads the first line of text from the given file.
+     * Reference {@link BufferedReader#readLine()} for clarification on what a line is
+     *
+     * @return the read line contents, or null on failure
+     */
+    public static String readOneLine(String fileName) {
+        String line = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName), 512);
+            line = reader.readLine();
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for reading", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not read from file " + fileName, e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return line;
+    }
+
+    /**
+     * Writes the given value into the given file
+     *
+     * @return true on success, false on failure
+     */
+    public static boolean writeLine(String fileName, String value) {
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(value);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write to file " + fileName, e);
+            return false;
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks whether the given file exists
+     *
+     * @return true if exists, false if not
+     */
+    public static boolean fileExists(String fileName) {
+        final File file = new File(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Checks whether the given file is readable
+     *
+     * @return true if readable, false if not
+     */
+    public static boolean isFileReadable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canRead();
+    }
+
+    /**
+     * Checks whether the given file is writable
+     *
+     * @return true if writable, false if not
+     */
+    public static boolean isFileWritable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canWrite();
+    }
+
+    /**
+     * Deletes an existing file
+     *
+     * @return true if the delete was successful, false if not
+     */
+    public static boolean delete(String fileName) {
+        final File file = new File(fileName);
+        boolean ok = false;
+        try {
+            ok = file.delete();
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+        }
+        return ok;
+    }
+
+    /**
+     * Renames an existing file
+     *
+     * @return true if the rename was successful, false if not
+     */
+    public static boolean rename(String srcPath, String dstPath) {
+        final File srcFile = new File(srcPath);
+        final File dstFile = new File(dstPath);
+        boolean ok = false;
+        try {
+            ok = srcFile.renameTo(dstFile);
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to rename " + srcPath + " to " + dstPath, e);
+        } catch (NullPointerException e) {
+            Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
+        }
+        return ok;
+    }
+}

ancient-devices/device_realme_RMX1851-common

RealmeHub/src/com/realme/hub/fpsinfo/FPSTileService.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (C) 2020 DerpFest ROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.realme.hub.fpsinfo;
+
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.os.Handler;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+// TODO: Add FPS drawables
+public class FPSTileService extends TileService {
+
+  private final String KEY_FPS_INFO = "fps_info";
+
+  private boolean isShowing = false;
+
+  public FPSTileService() { }
+
+  @Override
+  public void onStartListening() {
+      super.onStartListening();
+      ActivityManager manager =
+              (ActivityManager) getSystemService(this.ACTIVITY_SERVICE);
+      for (ActivityManager.RunningServiceInfo service :
+              manager.getRunningServices(Integer.MAX_VALUE)) {
+          if (FPSInfoService.class.getName().equals(
+                  service.service.getClassName())) {
+              isShowing = true;
+          }
+      }
+      updateTile();
+  }
+
+  @Override
+  public void onClick() {
+      Intent fpsinfo = new Intent(this, FPSInfoService.class);
+      if (!isShowing)
+          this.startService(fpsinfo);
+      else
+          this.stopService(fpsinfo);
+      isShowing = !isShowing;
+      updateTile();
+  }
+
+  private void updateTile() {
+      final Tile tile = getQsTile();
+      tile.setState(isShowing ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+      tile.updateTile();
+  }
+
+}

RealmeHub/src/com/realme/hub/util/FileUtils.java
@@ -1,0 +1,160 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.realme.hub.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class FileUtils {
+    private static final String TAG = "FileUtils";
+
+    private FileUtils() {
+        // This class is not supposed to be instantiated
+    }
+
+    /**
+     * Reads the first line of text from the given file.
+     * Reference {@link BufferedReader#readLine()} for clarification on what a line is
+     *
+     * @return the read line contents, or null on failure
+     */
+    public static String readOneLine(String fileName) {
+        String line = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName), 512);
+            line = reader.readLine();
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for reading", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not read from file " + fileName, e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return line;
+    }
+
+    /**
+     * Writes the given value into the given file
+     *
+     * @return true on success, false on failure
+     */
+    public static boolean writeLine(String fileName, String value) {
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(value);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write to file " + fileName, e);
+            return false;
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks whether the given file exists
+     *
+     * @return true if exists, false if not
+     */
+    public static boolean fileExists(String fileName) {
+        final File file = new File(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Checks whether the given file is readable
+     *
+     * @return true if readable, false if not
+     */
+    public static boolean isFileReadable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canRead();
+    }
+
+    /**
+     * Checks whether the given file is writable
+     *
+     * @return true if writable, false if not
+     */
+    public static boolean isFileWritable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canWrite();
+    }
+
+    /**
+     * Deletes an existing file
+     *
+     * @return true if the delete was successful, false if not
+     */
+    public static boolean delete(String fileName) {
+        final File file = new File(fileName);
+        boolean ok = false;
+        try {
+            ok = file.delete();
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+        }
+        return ok;
+    }
+
+    /**
+     * Renames an existing file
+     *
+     * @return true if the rename was successful, false if not
+     */
+    public static boolean rename(String srcPath, String dstPath) {
+        final File srcFile = new File(srcPath);
+        final File dstFile = new File(dstPath);
+        boolean ok = false;
+        try {
+            ok = srcFile.renameTo(dstFile);
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to rename " + srcPath + " to " + dstPath, e);
+        } catch (NullPointerException e) {
+            Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
+        }
+        return ok;
+    }
+}